3ee7f1ceef37e864c1f3584f6d8d099d91d37684
Alex Bush <abush@hortonworks.com>
2016-08-31 17:35:24 +0100

    various fixes to shrink VM size and create guest additions

--- centos-6.6/scripts/base.sh
+++ centos-6.6/scripts/base.sh
@@ -1,3 +1,5 @@
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
-yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl
-
+yum -y install gcc make gcc-c++ kernel-devel perl
+yum -y update
+reboot
+sleep 60
